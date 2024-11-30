417788752ed01f10a6a2a49fc5906565bbd6e619
Леонид Кухарчик <kuxarchik007@mail.ru>
2024-11-30 13:02:21 +0300

    Начинаю заново, так как в графах должны храниться имена в виде строки

--- Graph.h
+++ Graph.h
@@ -1,36 +1,10 @@
 #pragma once
-#define SIZE 10
+
+#include <vector>
+#include <string>
+
 
 class Graph
 {
-public:
-	Graph();
-	
-	// Добавление вершины
-	void addVetrex(int vnumber);
 
-	// Добавление ребра
-	void addEdge(Human *a, Human *p, int weight);
-
-	// Удаление вершины
-	void delVertex(int vnumber);
-
-	// Удаление ребра
-	void delEdge(int v1, int v2);
-
-private:
-	// Проверка существования ребра 
-	bool edgeExists(int v1, int v2);
-
-	// Проверка существования вершины
-	bool vertexExists(int v);
-
-	// Матрица
-	std::string matrix[SIZE][SIZE];
-
-	// Хранилище вершин
-	int vertexes[SIZE];
-
-	// Количество добавленых вершин
-	int vertexCount;
 };
